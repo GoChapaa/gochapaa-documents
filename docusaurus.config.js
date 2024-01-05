b2447fe7d20d00d5c29f3ec3b79ccf9447569a14
Ouda892 <152851725+Ouda892@users.noreply.github.com>
2024-01-05 10:53:08 +0300

    fix for github links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://gochapaa-documents.vercel.app',
+  url: 'https://gochapaa-documents-ten.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ouda892', // Usually your GitHub org/user name.
+  organizationName: 'schapaa', // Usually your GitHub org/user name.
   projectName: 'gochapaa-documents', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ouda892/gochapaa-documents/edit/main/',
+            'https://github.com/GoChapaa/gochapaa-documents/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Ouda892/gochapaa-documents/edit/main/',
+            'https://github.com/GoChapaa/gochapaa-documents/edit/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -80,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/ouda892/gochapaa-documents',
+            href: 'https://github.com/GoChapaa/gochapaa-documents',
             label: 'GitHub',
             position: 'right',
           },
